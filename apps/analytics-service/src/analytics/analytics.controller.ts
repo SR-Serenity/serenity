e7e2ce4bed22ff143d6f9a3afcc47aefdc27530e
Huy Ph <huy.pmg16@gmail.com>
2026-05-01 16:14:52 +0700

    chore: cleanup and miscellaneous updates

--- apps/analytics-service/src/analytics/analytics.controller.ts
+++ apps/analytics-service/src/analytics/analytics.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AnalyticsService } from './analytics.service';
 
+@ApiTags('analytics')
+@ApiBearerAuth()
 @Controller()
 export class AnalyticsController {
   constructor(private readonly analyticsService: AnalyticsService) {}
--- apps/realtime-service/src/realtime/realtime.controller.ts
+++ apps/realtime-service/src/realtime/realtime.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { RealtimeService } from './realtime.service';
 
+@ApiTags('realtime')
+@ApiBearerAuth()
 @Controller()
 export class RealtimeController {
   constructor(private readonly realtimeService: RealtimeService) {}
